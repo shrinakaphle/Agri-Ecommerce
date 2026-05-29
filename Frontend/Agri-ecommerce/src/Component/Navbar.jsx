049f997cca42a0a1fcfdaa77b2b4f5c3609d0bdd
shrinakaphle <shrinakaphle@gmail.com>
2026-05-29 23:04:25 +0545

    fotter section changed and Added things in homescreen

--- Frontend/Agri-ecommerce/src/Component/Navbar.jsx
+++ Frontend/Agri-ecommerce/src/Component/Navbar.jsx
@@ -1,9 +1,28 @@
+//  import logo from "../assets/logo.jpeg";
+import {useEffect,useState} from "react";
+// import"../assets/Css/logo.css";
 const Navbar = ()=>{
+    const[scrolled,setScrolled]=useState(false);
+    useEffect(()=>{
+        const handleScroll=()=>{
+            if(window.scrollY>50){
+                setScrolled(true);
+
+            }else{
+                setScrolled(false);
+            }
+        };
+        window.addEventListener("scroll",handleScroll);
+        return()=>window.removeEventListener("scroll",handleScroll);
+    },[]);
     return(
-        <nav className="navbar">
+        <nav className={`navbar ${scrolled? "scrolled":""}`}>
         <div className="logo">
+            {/* <img src={logo} alt="AMARSIDDHI Logo" className ="logo"/> */}
+            <div className ="logo-text">
             <h2>AMARSIDDHI</h2>
             <span>krishi Udhyog</span>
+        </div>
         </div>
 
         <ul className="nav-links">
