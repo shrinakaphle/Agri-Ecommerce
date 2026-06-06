5d474e714d7a216809a9b2d4d927b72feca0d056
shrinakaphle <shrinakaphle@gmail.com>
2026-06-06 21:37:47 +0545

    Categories added

--- Frontend/Agri-ecommerce/src/App.jsx
+++ Frontend/Agri-ecommerce/src/App.jsx
@@ -1,11 +1,8 @@
+// import{Routes,Route} from "react-router-dom";
 import Home from "./pages/Home";
 
-const App = () => {
-  return (
-    <div>
-      <Home />
-    </div>
-  );
-};
+function App() {
+  return <Home />;
+}
 
 export default App;
